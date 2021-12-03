ad381a8a247e14a7c40f0f9a88339ac1acc5fe09
Dennizz <root@ipaddr.nl>
2021-12-03 13:35:14 +0100

    fix firewall_name output

--- output.tf
+++ output.tf
@@ -35,5 +35,5 @@ output "firewall_instance_nic_names" {
 
 output "firewall_name" {
   description = "A list of the firewall names created"
-  value       = [for name in aviatrix_firenet.firenet.firewall_instance_association.*.instance_id : join("", regex("^(.*?):", name))]
+  value       = local.is_aviatrix ? null : (var.ha_gw ? [join("", regex("^(.*?):", aviatrix_firewall_instance.firewall_instance_1[0].name)), join("", regex("^(.*?):", aviatrix_firewall_instance.firewall_instance_2[0].name))] : [join("", regex("^(.*?):", aviatrix_firewall_instance.firewall_instance[0].name))])
 }

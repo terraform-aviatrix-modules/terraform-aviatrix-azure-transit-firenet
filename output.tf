f688978046ae63382b0c9df08c80987bdb055895
Dennizz <root@ipaddr.nl>
2021-12-03 13:43:04 +0100

    formatting

--- output.tf
+++ output.tf
@@ -35,5 +35,5 @@ output "firewall_instance_nic_names" {
 
 output "firewall_name" {
   description = "A list of the firewall names created"
-  value       = local.is_aviatrix ? null : (var.ha_gw ? [aviatrix_firewall_instance.firewall_instance_1[0].firewall_name, aviatrix_firewall_instance.firewall_instance_2[0].firewall_name]: [aviatrix_firewall_instance.firewall_instance[0].firewall_name])
+  value       = local.is_aviatrix ? null : (var.ha_gw ? [aviatrix_firewall_instance.firewall_instance_1[0].firewall_name, aviatrix_firewall_instance.firewall_instance_2[0].firewall_name] : [aviatrix_firewall_instance.firewall_instance[0].firewall_name])
 }
